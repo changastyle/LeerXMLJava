461c71bbf330ccc8d4f5ec806c6dbcb9887d83f6
Nicolas Grossi <nico.grossi4@gmail.com>
2015-06-30 17:59:51 -0300

    Se borraron comentarios

--- src/leerxml/HandlerXML.java
+++ src/leerxml/HandlerXML.java
@@ -35,26 +35,6 @@ public class HandlerXML
                                 Element element = (Element) nList.item(0);
                                 
                                 respuesta = "" +  element.getElementsByTagName(tag).item(0).getTextContent() ;
-                                //System.out.println("----------------------------");
-                                /*
-                                for (int temp = 0; temp < nList.getLength(); temp++) {
- 
-		Node nNode = nList.item(temp);
- 
-		System.out.println("\nCurrent Element :" + nNode.getNodeName());
- 
-		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
- 
-			Element eElement = (Element) nNode;
- 
-			System.out.println("Staff id : " + eElement.getAttribute("id"));
-			System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
-			System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
-			System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
-			System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
- 
-		}
-	}*/
                         }
                         catch(Exception e)
                         {
@@ -64,7 +44,7 @@ public class HandlerXML
                 }
                 else
                 {
-                        System.out.println("NO EXISTE EL ARCHIVO.");
+                        System.out.println("ERROR: NO EXISTE EL ARCHIVO.");
                 }
                 return respuesta;
         }
